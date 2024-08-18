f0e4cc79696c1f1d5aadfb53b5c4480cbcd9033b
Rheyles <148101401+Rheyles@users.noreply.github.com>
2024-08-18 16:00:11 +0200

    Add Music player Anim + Memo

---
+++ Scenes/UI/MainUI.gd
@@ -0,0 +1,7 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$"Music Player/AudioBars/AnimationPlayer".play("idle")
+	$WinJauge/AnimationPlayer.play("idle")
